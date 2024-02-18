33040decd58da9cfdcd186d597c9d81e5f96dd3e
Harold-coder <hjc2154@columbia.edu>
2024-02-18 18:17:28 -0500

    Changed the table

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const tableName = process.env.CONNECTIONS_TABLE;
 
 exports.handler = async (event) => {
-  const gameID = event.requestContext.gameID;
+  const connectionId = event.requestContext.connectionId;
 
   try {
-    await dynamoDb.delete({ TableName: tableName, Key: { gameID: gameID } }).promise();
+    await dynamoDb.delete({ TableName: tableName, Key: { connectionId: connectionId } }).promise();
     return { statusCode: 200, body: 'Disconnected.' };
   } catch (err) {
     console.error('Error removing connection:', err);
