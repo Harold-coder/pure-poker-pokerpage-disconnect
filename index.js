4b28bd195e557ef347b3e8e7dd8f0c3a9676d076
Harold-coder <hjc2154@columbia.edu>
2024-04-11 10:13:40 -0400

    Now invoking leaveGame as well

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const AWS = require('aws-sdk');
+const lambda = new AWS.Lambda();
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const tableName = process.env.CONNECTIONS_TABLE;
 
@@ -6,6 +7,30 @@ exports.handler = async (event) => {
     const connectionId = event.requestContext.connectionId;
 
     try {
+        // First, get the connection to check if there's a gameId associated with it
+        const getResult = await dynamoDb.get({
+            TableName: tableName,
+            Key: { connectionId: connectionId }
+        }).promise();
+
+        const connectionItem = getResult.Item;
+
+        if (connectionItem && connectionItem.gameId) {
+            // If there's a gameId, invoke the leaveGame function
+            console.log(`Connection ${connectionId} has gameId ${connectionItem.gameId}, invoking leaveGame function.`);
+            
+            await lambda.invoke({
+                FunctionName: 'poker-page-leaveGame',
+                InvocationType: 'Event', // Use 'Event' to invoke the function asynchronously
+                Payload: JSON.stringify({
+                    gameId: connectionItem.gameId,
+                    playerId: connectionItem.playerId
+                })
+            }).promise();
+
+            console.log(`leaveGame invoked for gameId ${connectionItem.gameId} and playerId ${connectionItem.playerId}.`);
+        }
+
         // Attempt to delete the connection
         const deleteResult = await dynamoDb.delete({
             TableName: tableName,
